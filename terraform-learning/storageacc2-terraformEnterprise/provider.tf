9921ef5e932589d289d5d5804cfa981a6d294c4e
hundal5 <45250443+hundal5@users.noreply.github.com>
2023-06-07 00:24:45 -0400

    h

---
+++ terraform-learning/storageacc2-terraformEnterprise/provider.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+    features {
+ }
+  
+}
+#{
+#  "appId": "13f58a9d-f921-4b9c-8d53-bf1993834a23",
+ #"displayName": "azure-cli-2023-06-06-02-27-13",
+ # "password": "jTb8Q~ve~a5fyFmA3dpPFzdT2fOnM1wmrPn_XcOY",
+  #"tenant": "b079fc7c-6ddb-45d3-826c-d07b773ccabe"
+#}
+
+#Token
+#Pii4OWOuMyGp8Q.atlasv1.HUnnCSnHcXmyP32utRZ7fmBJXNPAHHhlAFkH7icPJWBilXp2iZGIHfyxOlDlmncu2sQ
+#LaG1xTrziQ7Sjw.atlasv1.9MenGZFCAU0VmtymL7uBeQS3oJhvrpGSB0gqeInjZUTJ5OX7FYOOrnbPq8umoc2yhgM
